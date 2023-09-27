927fb64aaa7253179d455f5cf6b590ed61dc3c6c
Eduardo Vieira <eduardo.vieira@maxmilhas.com.br>
2023-09-27 14:03:53 -0300

    initial commit

---
+++ src/Api/PeopleEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Models
+{
+    public class PessoaEntity
+    {
+
+        public Guid Id { get; set; }
+        public required string Apelido { get; set; }
+        public required string Nome { get; set; }
+        public DateTime? Nascimento { get; set; }
+        public required string[] Stack { get; set; }
+
+        
+    }
+}
